1361f73dfe750f5fb45fbdd5bcb3d0219dcbf071
unknown <iemmaenebeli@gmail.com>
2022-01-12 21:09:46 +0100

    completed

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,7 +4,6 @@ const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
 const navItems = document.querySelectorAll('.nav-item');
-
 // Set Initial State Of Menu
 let showMenu = false;
 
@@ -30,4 +29,6 @@ function toggleMenu() {
     // Set Menu State
     showMenu = false;
   }
+
+ 
 }
