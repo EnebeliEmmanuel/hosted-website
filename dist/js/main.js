3e8e7482d9ab38cfb19316e0cd42cb83bb465ef5
unknown <iemmaenebeli@gmail.com>
2022-01-31 13:18:29 +0100

    rebranded

--- dist/js/main.js
+++ dist/js/main.js
@@ -30,5 +30,11 @@ function toggleMenu() {
     showMenu = false;
   }
 
- 
+  document.querySelector('#contact-form').addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.target.elements.name.value = '';
+    e.target.elements.email.value = '';
+    e.target.elements.message.value = '';
+  });
+
 }
